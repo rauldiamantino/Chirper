719445afbd23ce63096ea9f95d35a2db8d90397f
Raul Diamantino <rauldiamantino25@gmail.com>
2024-02-18 11:56:22 -0300

    Feat: Deleting Chirps

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -38,6 +38,14 @@
                                         <x-dropdown-link :href="route('chirps.edit', $chirp)">
                                             {{ __('Edit') }}
                                         </x-dropdown-link>
+                                        <form method="POST" action="{{ route('chirps.destroy', $chirp) }}">
+                                            @csrf
+                                            @method('delete')
+                                            <x-dropdown-link :hred="route('chirps.destroy', $chirp)" onclick="event.preventDefault(); this.closest('form').submit();">
+                                                {{ __('Delete') }}
+                                            </x-dropdown-link>
+
+                                        </form>
                                     </x-slot>
                                 </x-dropdown>
                             @endif
