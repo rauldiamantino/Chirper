e66bb72cd90e222fba7769f41c0e5e84d757807e
Raul Diamantino <rauldiamantino25@gmail.com>
2024-02-19 19:59:56 -0300

    Feat: Sending Notifications

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Chirp extends Model
 {
